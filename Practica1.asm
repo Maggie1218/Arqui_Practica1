8e75442074b9c495001a7c7ec71b7f5799ab6eb3
Margarita Jauregui Franco <margarita.jaureguif@gmail.com>
2018-02-19 16:31:53 -0600

    Move

--- Practica1.asm
+++ Practica1.asm
@@ -4,3 +4,18 @@ auxiliar:  .word 0 0 0 0 0
 destino: .word 0 0 0 0 0
 n: .word 5
 .text
+
+
+
+
+
+
+
+
+move: 
+	lw $s0, 0($a0)			#carga en s0 el contenido de la direccion a0 (la parte mas alta de la torre de origen
+	addi $s1, $zero, 0		
+	sw $s1, 0($a0)			#pone como vacia la direccion de donde se tomo el elemento a mover
+	addi $a0, $a0, -4		#mueve la direccion "tope" de la torre de origen una posicion menos
+	sw $s0, 0($a1)			#guarda el valor tomado de la torre de origen en el tope de la torre de destino
+	jr $ra
