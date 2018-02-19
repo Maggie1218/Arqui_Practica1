7272dd2b492ddaa6527af1fbde8bfe09af8a0332
Margarita Jauregui Franco <margarita.jaureguif@gmail.com>
2018-02-19 17:07:32 -0600

    Funcion de Hanoi

--- Practica1.asm
+++ Practica1.asm
@@ -12,7 +12,26 @@ n: .word 5
 
 
 
-move: 
+
+
+
+
+
+
+
+
+Hanoi:
+	beq $a0, 1, base		#si n==1, vamos al caso base
+	addi $a0, $a0, -1		#
+	jal Hanoi
+	jal Move
+	jal Hanoi
+	
+
+
+
+
+Move: 
 	lw $s0, 0($a0)			#carga en s0 el contenido de la direccion a0 (la parte mas alta de la torre de origen
 	addi $s1, $zero, 0		
 	sw $s1, 0($a0)			#pone como vacia la direccion de donde se tomo el elemento a mover
