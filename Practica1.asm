06034c2ca94e33b2af8ef9a385bf260752d3f780
Margarita Jauregui Franco <margarita.jaureguif@gmail.com>
2018-02-19 19:02:43 -0600

    Funcion de Hanoi con recursividad

--- Practica1.asm
+++ Practica1.asm
@@ -23,8 +23,26 @@ n: .word 5
 Hanoi:
 	beq $a0, 1, base		#si n==1, vamos al caso base
 	addi $a0, $a0, -1		#
+	addi $sp, $sp, -20
+	sw $a0, 0($sp)
+	sw $a1, 4($sp)			#a1 = org
+	sw $a2, 8($sp)			#a2 = aux
+	sw $a3, 12($sp)			#a3 = dest
+	sw $ra, 16($sp)
+	add $t0, $a0, $zero		#respaldar valores en registros auxiliares
+	add $t1, $a1, $zero		#para poder pasarlos posteriomente 
+	add $t2, $a2, $zero		#como argumentos en el
+	add $t3, $a3, $zero
+	add $a2, $t3, $zero		#a2 = dest
+	add $a3, $t2, $zero		#a3 = aux
 	jal Hanoi
+	add $a0, $t1, $zero
+	add $a1, $t3, $zero
 	jal Move
+	add $a0, $t0, $zero
+	add $a1, $t1, $zero
+	add $a2, $t2, $zero
+	add $a3, $t3, $zero 
 	jal Hanoi
 	
 
