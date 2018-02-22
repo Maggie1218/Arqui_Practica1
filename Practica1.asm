58d2b5ef467df5086ec3cf36382918cf2b3593f1
Andrea Rayon <andrearymr@gmail.com>
2018-02-22 17:28:48 -0600

    Correción del argumento de torre a , Ya funciona :')

--- Practica1.asm
+++ Practica1.asm
@@ -1,34 +1,33 @@
 .data
-origen: .word 5 4 3 2 1
-auxiliar:  .word 0 0 0 0 0
-destino: .word 0 0 0 0 0
 n: .word 5
 .text
-
-
-
+Main:
+	add $a1, $zero, 0x1001		#carga los 4lsb de la direccion de la torre A
+	sll $a1, $a1, 16		#recorre logicamente 4 posiciones cada bit para hacerlos msb
+	add $a2, $zero, $a1		#copia lo obtenido en a1 a a2
+	ori $a2, 0x0004			#hace or con 4 para moverse a la direccion de inicio de la torre B
+	add $a3, $zero, $a1		#pasa los mismos 4msb a a3
+	ori $a3, 0x0008			#hace or con 8 para moverse a la direccion de inicio de la torre C
+	lw $a0, n
+	lw $s0, n
 
 
 Torre: 
-	beq $a0, $zero, Hanoi 		#Si el numero de discos es == 0, salta a hanoi
-	sw $a0, ($a1)			#cargamos el numero de discos a la torre origen 
-	sub  $a0, $a0, 1		#Restamos un disco
-	addi  $a1, $a1, 4		#Recorremos 
+	beq $s0, $zero, Hanoi 		#Si el numero de discos es == 0, salta a hanoi
+	sw $s0, 0($a1)			#cargamos el numero de discos a la torre origen 
+	sub  $s0, $s0, 1		#Restamos un disco
+	addi  $a1, $a1, 0x20		#Recorremos 
 	j Torre
-
 
 Hanoi:
 	beq $a0, 1, base		#si n==1, vamos al caso base
 	addi $a0, $a0, -1		#
 	addi $sp, $sp, -20
-	sw $a0, 0($sp)			#a0 = n (numero de discos) 
+	sw $a0, 0($sp)
 	sw $a1, 4($sp)			#a1 = org
 	sw $a2, 8($sp)			#a2 = aux
 	sw $a3, 12($sp)			#a3 = dest
 	sw $ra, 16($sp)
-	
-	
-
 	add $t0, $a0, $zero		#respaldar valores en registros auxiliares
 	add $t1, $a1, $zero		#para poder pasarlos posteriomente 
 	add $t2, $a2, $zero		#como argumentos en el
@@ -36,36 +35,29 @@ Hanoi:
 	add $a2, $t3, $zero		#a2 = dest
 	add $a3, $t2, $zero		#a3 = aux
 	jal Hanoi
-	
 	add $a0, $t1, $zero
 	add $a1, $t3, $zero
 	jal Move
-	
 	add $a0, $t0, $zero
 	add $a1, $t1, $zero
 	add $a2, $t2, $zero
 	add $a3, $t3, $zero 
 	jal Hanoi
 	
-#base: 
-#	jal Move
-#	j exitHanoi
-	
+base:
+	add $a0, $t1, $zero
+	add $a1, $t3, $zero
+	jal Move
+	j exitRecursividad
 
-exitHanoi: 
-	lw $a0, 0($sp)
-	lw $a1, 4($sp)
-	lw $a2, 8($sp)
-	lw $a3, 12($sp)
-	lw $ra, 16($sp)
-	addi $sp, $sp, -20
-	jr $ra
-	
+
 Move: 
 
 	lw $s0, 0($a0)			#carga en s0 el contenido de la direccion a0 (la parte mas alta de la torre de origen
-	addi $s1, $zero, 0		
+	addi $s1, $zero, 4		
 	sw $s1, 0($a0)			#pone como vacia la direccion de donde se tomo el elemento a mover
-	addi $a0, $a0, -4		#mueve la direccion "tope" de la torre de origen una posicion menos
+	addi $a0, $a0, -20		#mueve la direccion "tope" de la torre de origen una posicion menos
 	sw $s0, 0($a1)			#guarda el valor tomado de la torre de origen en el tope de la torre de destino
 	jr $ra
+	
+exitRecursividad:
